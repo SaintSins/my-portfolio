a7a1b649d5b082a1832325060ae397f32d87a903
SaintSins <107116997+SaintSins@users.noreply.github.com>
2026-06-10 00:34:13 +0530

    Initial Setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
+  site: 'https://saint-sins.github.io',
+  base: '/my-portfolio',
   integrations: [mdx(), sitemap(), tailwind()]
 });
