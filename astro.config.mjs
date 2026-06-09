b0304a9de2dd34471cf633348692e1fd8cabbcc9
SaintSins <107116997+SaintSins@users.noreply.github.com>
2026-06-10 00:59:04 +0530

    fix: remove base path for root deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://saint-sins.github.io',
-  base: '/my-portfolio',
   integrations: [mdx(), sitemap(), tailwind()]
 });
